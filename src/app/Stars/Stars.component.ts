eb051b848625517837306e1f008d5822ffdabf7c
Hector Gonzalez Viejo <hector.gv00@gmail.com>
2023-09-24 20:35:51 +0200

    [documentacion]

--- src/app/Stars/Stars.component.ts
+++ src/app/Stars/Stars.component.ts
@@ -27,22 +27,19 @@ export class StarsComponent implements OnInit {
   ngOnChanges(changes: SimpleChanges) {
     if (changes['stars']) {
       if (changes['stars'].currentValue) {
+        // Añadimos un timeout de una milesima por desfase
         setTimeout(() => {
-          let starsContainer = document.querySelector(`#${this.stars.name}`);
-          for (let index = 0; index < this.stars.stars * 2; index++) {
-            let halfStar = document.createElement(`input`);
-            halfStar.setAttribute('type', 'radio');
-            halfStar.setAttribute('name', this.stars.name);
-            starsContainer?.appendChild(halfStar);
-          }
+          // Recogemos el contenedor de las estrellas que coincida con el nombre
+          let starsContainer: HTMLElement = this.getStarsContainer();
+          // Creamos los inputs de las estrellas
+          this.createInputsStars(starsContainer)
+          // Recogemos los inputs de las estrellas
           let stars:HTMLElement[] = Array.from(document.querySelectorAll(
             `input[type="radio"][name="${this.stars.name}"]`
-          ));          
-          stars.forEach((star, index) => {
-            star.addEventListener('click', () => {
-              this.rating.emit(this.stars.stars * 2 - index);
-            });
-          });
+          ));      
+          // Añadimos el evento click a cada estrella
+          this.addEventListenerClick(stars);    
+          // Si hay una valoracion previa, la mostramos
           if(this.stars.rating){
             stars[this.stars.stars * 2 - this.stars.rating].click();
           }
@@ -53,4 +50,24 @@ export class StarsComponent implements OnInit {
 
   ngOnInit() {}
   
+  createInputsStars(starsContainer:HTMLElement ){
+    for (let index = 0; index < this.stars.stars * 2; index++) {
+      let halfStar = document.createElement(`input`);
+      halfStar.setAttribute('type', 'radio');
+      halfStar.setAttribute('name', this.stars.name);
+      starsContainer?.appendChild(halfStar);
+    }
+  }
+
+  addEventListenerClick(stars:HTMLElement[]){
+    stars.forEach((star, index) => {
+      star.addEventListener('click', () => {
+        this.rating.emit(this.stars.stars * 2 - index);
+      });
+    });
+  }
+
+  getStarsContainer():HTMLElement{
+   return document.querySelector(`#${this.stars.name}`)!;
+  }
 }
